404b8e6afbbbfa400fb10ed38fe352e33e9703d2
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-04-02 18:18:15 +0300

    Реализован переход EditText с одной активности во TextView в другой активности

--- app/src/main/java/com/example/lecturethirteen/MainActivity.java
+++ app/src/main/java/com/example/lecturethirteen/MainActivity.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static String KEY = "KEY";
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.viewha);
+        findViewById(R.id.transfer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+                intent.putExtra(KEY, editText.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lecturethirteen/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.lecturethirteen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.security.Key;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView transferedVeiw;
+    String transferedString;
+    Intent transferedIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        transferedVeiw = findViewById(R.id.veiw);
+        transferedIntent = getIntent();
+        transferedString = transferedIntent.getStringExtra(MainActivity.KEY);
+        transferedVeiw.setText(transferedString);
+
+       // ((TextView)findViewById(R.id.veiw)).setText(getIntent().getStringExtra(MainActivity.KEY));
+    }
+}
